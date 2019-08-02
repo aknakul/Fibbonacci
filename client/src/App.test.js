35030960d08b705e077348a931936127844675e4
arvind412 <arvind.mehta12@gmail.com>
2019-08-02 15:06:41 +0530

    test clean up

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
 it('renders without crashing', () => {});
